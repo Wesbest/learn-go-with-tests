e5dc2d70cbb2ea3237a956d54b9f911c944fd95a
wesleyrouw <wesley.rouw@devoteam.com>
2022-03-18 13:54:45 +0100

    creating incremental counter

--- chapter13/main.go
+++ chapter13/main.go
@@ -1,18 +1,15 @@
 package main
 
-import "sync"
-
 type Counter struct {
-	mu    sync.Mutex
 	value int
 }
 
+// Inc the count.
 func (c *Counter) Inc() {
-	c.mu.Lock()
-	defer c.mu.Unlock()
 	c.value++
 }
 
+// Value returns the current count.
 func (c *Counter) Value() int {
 	return c.value
 }
