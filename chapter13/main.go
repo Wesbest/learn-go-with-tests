ea86875edf7c0d43c8b63807ef276e26e6029b8b
wesleyrouw <wesley.rouw@devoteam.com>
2022-03-18 14:04:13 +0100

    added mutex to counter function

--- chapter13/main.go
+++ chapter13/main.go
@@ -1,11 +1,16 @@
 package main
 
+import "sync"
+
 type Counter struct {
+	mu    sync.Mutex
 	value int
 }
 
 // Inc the count.
 func (c *Counter) Inc() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.value++
 }
 
